4c6eef97f872ef161bcfc10a0c1176a0a3002b67
181250178@smail.nju.edu.cn <181250178@smail.nju.edu.cn>
2021-04-07 10:31:21 +0800

    数据校验

--- src/main/java/com/example/web/LoginController.java
+++ src/main/java/com/example/web/LoginController.java
@@ -1,17 +1,26 @@
 package com.example.web;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.Errors;
+import org.springframework.validation.ObjectError;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.example.domain.User;
 import com.example.service.UserService;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
+
+import static org.springframework.web.bind.annotation.RequestMethod.POST;
 
 @Controller
 @RequestMapping(value = "/admin")
@@ -29,8 +38,20 @@ public class LoginController {
         return "login";
     }
 
-    @RequestMapping(value = "/loginCheck.html")
-    public ModelAndView loginCheck(HttpServletRequest request, LoginInfo loginInfo) {
+    @RequestMapping(value = "/loginCheck.html",method = POST)
+    public ModelAndView loginCheck(HttpServletRequest request, @Valid LoginInfo loginInfo, BindingResult errors) {
+        System.out.println("hello");
+        System.out.println(errors);
+        System.out.println(loginInfo);
+        if(errors.hasErrors()){
+
+            List<ObjectError> myErrorList=errors.getAllErrors();
+            StringBuilder sb=new StringBuilder();
+            for (int i=0;i<myErrorList.size();i++){
+                sb.append(myErrorList.get(i).getDefaultMessage()).append("\n");
+            }
+            return new ModelAndView("login","error",sb.toString());
+        }
         boolean isValidUser =
                 userService.hasMatchUser(loginInfo.getUserName(),
                         loginInfo.getPassword());
--- src/main/java/com/example/web/LoginInfo.java
+++ src/main/java/com/example/web/LoginInfo.java
@@ -1,9 +1,17 @@
 package com.example.web;
 
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 public class LoginInfo {
 
+    @NotNull
+    @Size(min=3, max=6, message="用户名长度范围为3-6个字符")
     private String userName;
 
+    @NotNull
+    @Size(min=6, max=6, message="密码长度为6")
     private String password;
 
     public String getPassword() {
